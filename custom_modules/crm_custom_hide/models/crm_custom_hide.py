46b569dbe31808b78f145a36a507eee6c994af9f
Andres <acastillo@develoop.net>
2020-09-23 17:28:35 -0400

    Correccion cuando se crea sin usuario

--- custom_modules/crm_custom_hide/models/crm_custom_hide.py
+++ custom_modules/crm_custom_hide/models/crm_custom_hide.py
@@ -8,6 +8,7 @@ class crm_custom_hide(models.TransientModel):
     _name = 'crm.lead2opportunity.partner.custom'
     _inherit = 'crm.lead2opportunity.partner'
 
+
     name = fields.Selection([
         ('convert', 'Convert to opportunity'),
         # ('merge', 'Merge with existing opportunities')
@@ -18,3 +19,40 @@ class crm_custom_hide(models.TransientModel):
         ('exist', 'Link to an existing customer'),
         ('nothing', 'Do not link to a customer')
     ], 'Related Customer', required=True)
+
+    @api.model
+    def default_get(self, fields):
+        """ Default get for name, opportunity_ids.
+            If there is an exisitng partner link to the lead, find all existing
+            opportunities links with this partner to merge all information together
+        """
+        result = super(crm_custom_hide, self).default_get(fields)
+        if self._context.get('active_id'):
+            tomerge = {int(self._context['active_id'])}
+
+            partner_id = result.get('partner_id')
+            lead = self.env['crm.lead'].browse(self._context['active_id'])
+            email = lead.partner_id.email if lead.partner_id else lead.email_from
+
+            tomerge.update(self._get_duplicated_leads(partner_id, email, include_lost=True).ids)
+
+            if 'action' in fields and not result.get('action'):
+                result['action'] = 'exist' if partner_id else 'create'
+            if 'partner_id' in fields:
+                result['partner_id'] = partner_id
+            if 'name' in fields:
+                result['name'] = 'convert'#'merge' if len(tomerge) >= 2 else 'convert'
+            if 'opportunity_ids' in fields and len(tomerge) >= 2:
+                result['opportunity_ids'] = list(tomerge)
+            if lead.user_id:
+                result['user_id'] = lead.user_id.id
+            if lead.team_id:
+                result['team_id'] = lead.team_id.id
+            if not partner_id and not lead.contact_name:
+                result['action'] = 'nothing'
+        return result
+
+    @api.model
+    def _get_duplicated_leads(self, partner_id, email, include_lost=False):
+        """ Search for opportunities that have the same partner and that arent done or cancelled """
+        return self.env['crm.lead']._get_duplicated_leads_by_emails(partner_id, email, include_lost=include_lost)
