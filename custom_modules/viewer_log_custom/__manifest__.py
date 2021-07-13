6b10a77cd2f872a7e149ba22f14995c54b9008dd
acastillo456 <acastillo@develoop.net>
2021-07-13 08:39:45 -0400

    Check error

---
+++ custom_modules/viewer_log_custom/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Ver errores en log Custom",
+    'version': '0.1',
+    'author': "Develoop Software",
+    'category': 'Uncategorized',
+    'summary': 'Ver errores en log custom',
+    'website': "https://www.develoop.net/",
+    'description': """
+        Ver errores en log custom
+        """,
+    'depends': ['reporting-engine'],
+    'data': [
+        'views/sale_order_views.xml',
+    ],
+    'demo': [],
+    'installable': True,
+}
